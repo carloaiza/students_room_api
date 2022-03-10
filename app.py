22c844cd332667429941374a5859ef850adb2382
Carlos Loaiza <carloaiza@umanizales.edu.co>
2022-03-10 10:55:49 -0500

    Se retira método Hola mundo

--- app.py
+++ app.py
@@ -5,9 +5,5 @@ from controller.student_controller import app_student
 app = Flask(__name__)
 app.register_blueprint(app_student)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
 if __name__ == '__main__':
     app.run()
